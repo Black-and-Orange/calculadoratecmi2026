5a932e184dcc1e44d16310ddce2c213764b02bd3
Vanessa Gustin <vanesagustin98@hotmail.com>
2024-08-26 23:13:10 -0500

    Update dbConfig.js

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -1,19 +1,20 @@
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: 'database_calculadora'
+    database: 'database_calculadora',
+    connectionLimit: 10
 
 });
 
-db.connect(err => {
-    if (err) {
-        console.error('Error connecting to the database:', err);
-        return;
-    }
-    console.log('Connected to the database.');
-});
+// db.connect(err => {
+//     if (err) {
+//         console.error('Error connecting to the database:', err);
+//         return;
+//     }
+//     console.log('Connected to the database.');
+// });
 
 module.exports = db;
